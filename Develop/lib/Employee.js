c45b93427b8f72b663d7c3d4b0f05ec4886e3495
David Vargas <davidvargas95@gmail.com>
2021-03-11 20:50:08 -0800

    position constructors made

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,27 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.position = "Employee";
+    }
+
+    newName() {
+        return this.name;
+    }
+
+    newID() {
+        return this.id;
+    }
+
+    newEmail() {
+        return this.email;
+    }
+
+    newPosition() {
+        return this.position;
+    }
+}
+
+module.exports = Employee;
