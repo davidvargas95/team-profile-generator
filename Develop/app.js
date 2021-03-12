6ed8fbfd4da2f725f165399e5684483c473a5b72
David Vargas <davidvargas95@gmail.com>
2021-03-11 20:30:59 -0800

    app.js almost done.

--- Develop/app.js
+++ Develop/app.js
@@ -9,27 +9,138 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const { networkInterfaces } = require("os");
+const { createInflate } = require("zlib");
+
+const employeeList = [];
 
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+const generalQuestions = [
+    {
+        type: "input",
+        name: "name",
+        message: "Enter the name of this employee."
+    },
+    {
+        type: "input",
+        name: "ID",
+        message: "What is this employee's ID?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is this employee's email?"
+    },
+    {
+        type: "input",
+        name: "position",
+        message: "What is this employee's name?"
+    },
+];
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+employeePosition();
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+function employeePosition() {
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+    inquirer.prompt({
+        message: "Employee's position?",
+        name:"position",
+        type:"list",
+        choices: ["Engineer", "Intern", "Manager"]
+
+    }).then((response) => {
+        if (response.position === "Engineer") {
+            engineerPosition()
+        } else if (response.position === "Intern") {
+            internPosition();
+        } else if (response.position === "Manager") {
+            managerPosition();
+        }
+    });
+
+    function engineerPosition() {
+
+        inquirer.prompt([
+            ...generalQuestions,
+            {
+                type:"input",
+                message: "What is their github username?",
+                name: "github"
+            }
+        ])
+        .then(({name, id, email, github}) => {
+            employeeList.push(new Engineer(name, id, email, github));
+            //(response.name, response.id, response.email, response.github)
+            newEmployee();
+        })
+    }
+
+    function internPosition() {
+
+        inquirer.prompt([
+            ...generalQuestions,
+            {
+                type:"input",
+                message:"What school are they going to?",
+                name: "school"
+            }
+        ])
+        .then(({name, id, email, school}) => {
+            employeeList.push(new Intern(name, id, email, school));
+
+            newEmployee();
+        })
+    }
+
+    function managerPosition() {
+
+        inquirer.prompt([
+            ...generalQuestions,
+            {
+                type:"input",
+                message: "What is their phone number?",
+                name: "phone"
+            }
+        ])
+        .then(({name, id, email, phone}) => {
+            employeeList.push(new Manager(name, id, email, phone));
+
+            newEmployee();
+        })
+    }
+
+    function newEmployee() {
+        inquirer.prompt({
+            message: "Will you add another member?",
+            name: "another",
+            type: "list",
+            choices: [
+                "yes",
+                "no"
+            ],
+        }).then(({another}) => {
+            if (another === "yes") {
+                employeePosition();
+            } else {
+                console.log("Profiles successfully generated");
+                console.log(employeeList);
+                createFile();
+            }
+            
+        })
+        }
+    }
+
+    function createFile() {
+        const html = render(employeeList);
+        if (! fs.existsSync(OUTPUT_DIR))
+        fs.mkdirSync(OUTPUT_DIR);
+
+        fs.writeFile(outputPath, html, (error) => {
+            if (error) console.log(error);
+            else console.log("Page created");
+        });
+    }
